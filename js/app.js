7674e9122b9e2f5015b55f5b2753d427f2229d3f
Calvin G Hall <123chall@gmail.com>
2019-07-25 13:29:37 -0500

    Fixed bad design, added Cart

--- js/app.js
+++ js/app.js
@@ -3,39 +3,48 @@
 'use strict';
 
 // TODO: Create a "Cart" constructor (named Cart) that has item and quantity
-// properties, and builds an array of items as you create instances
+// properties, and builds an array of items as you create instances <--- NO!!!
+//You should NEVER do this, unless you have a damn good reason.
 
-
+//This classes name is horrid. It is not a cart, but a "CartItem".
+class Cart {
+  constructor(product, quantity) {
+    this.product = product;
+    this.quantity = quantity;
+  }
+}
 
 // Product Contructor
-var Product = function(filePath, name) {
-  this.filePath = filePath;
-  this.name = name;
-  Product.allProducts.push(this);
-};
-Product.allProducts = [];
+class Product {
+  constructor(filePath, name) {
+    this.filePath = filePath;
+    this.name = name;
+  }
+}
+
+var allProducts = [];
 
 function generateCatalog() {
-  new Product('assets/bag.jpg', 'Bag');
-  new Product('assets/banana.jpg', 'Banana');
-  new Product('assets/bathroom.jpg', 'Bathroom');
-  new Product('assets/boots.jpg', 'Boots');
-  new Product('assets/breakfast.jpg', 'Breakfast');
-  new Product('assets/bubblegum.jpg', 'Bubblegum');
-  new Product('assets/chair.jpg', 'Chair');
-  new Product('assets/cthulhu.jpg', 'Cthulhu');
-  new Product('assets/dog-duck.jpg', 'Dog-Duck');
-  new Product('assets/dragon.jpg', 'Dragon');
-  new Product('assets/pen.jpg', 'Pen');
-  new Product('assets/pet-sweep.jpg', 'Pet Sweep');
-  new Product('assets/scissors.jpg', 'Scissors');
-  new Product('assets/shark.jpg', 'Shark');
-  new Product('assets/sweep.png', 'Sweep');
-  new Product('assets/tauntaun.jpg', 'Taun-Taun');
-  new Product('assets/unicorn.jpg', 'Unicorn');
-  new Product('assets/usb.gif', 'USB');
-  new Product('assets/water-can.jpg', 'Water Can');
-  new Product('assets/wine-glass.jpg', 'Wine Glass');
+  allProducts.push(new Product('assets/bag.jpg', 'Bag'));
+  allProducts.push(new Product('assets/banana.jpg', 'Banana'));
+  allProducts.push(new Product('assets/bathroom.jpg', 'Bathroom'));
+  allProducts.push(new Product('assets/boots.jpg', 'Boots'));
+  allProducts.push(new Product('assets/breakfast.jpg', 'Breakfast'));
+  allProducts.push(new Product('assets/bubblegum.jpg', 'Bubblegum'));
+  allProducts.push(new Product('assets/chair.jpg', 'Chair'));
+  allProducts.push(new Product('assets/cthulhu.jpg', 'Cthulhu'));
+  allProducts.push(new Product('assets/dog-duck.jpg', 'Dog-Duck'));
+  allProducts.push(new Product('assets/dragon.jpg', 'Dragon'));
+  allProducts.push(new Product('assets/pen.jpg', 'Pen'));
+  allProducts.push(new Product('assets/pet-sweep.jpg', 'Pet Sweep'));
+  allProducts.push(new Product('assets/scissors.jpg', 'Scissors'));
+  allProducts.push(new Product('assets/shark.jpg', 'Shark'));
+  allProducts.push(new Product('assets/sweep.png', 'Sweep'));
+  allProducts.push(new Product('assets/tauntaun.jpg', 'Taun-Taun'));
+  allProducts.push(new Product('assets/unicorn.jpg', 'Unicorn'));
+  allProducts.push(new Product('assets/usb.gif', 'USB'));
+  allProducts.push(new Product('assets/water-can.jpg', 'Water Can'));
+  allProducts.push(new Product('assets/wine-glass.jpg', 'Wine Glass'));
 }
 
 // Initialize the app by creating the big list of products with images and names
